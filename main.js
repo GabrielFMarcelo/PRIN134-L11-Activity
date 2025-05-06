b6ec2cacb4ac21cb3b819d0a73b00ca60d9a80b9
Gabriel Marcelo <marcelo2235888@mls.ceu.edu.ph>
2025-05-06 12:17:37 +0800

    Finals-Reviewer

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 const gameArea = document.getElementById('gameArea');
-const target = document.getElementById('target');
 const scoreBoard = document.getElementById('scoreBoard');
 
 let score = 0;
+const array = [];
 
-function moveTarget() {
+function newTarget() {
+  const inputElement = document.getElementById("input");
+  const inputValue = inputElement.value;
+  gameArea.innerHTML = "";
+
+  for (let i = 0; i < inputValue; i++) {
+    const target = document.createElement("div");
+    target.classList.add("target")
+    target.textContent = i;
+
+    target.addEventListener('contextmenu', function(event) {
+      event.preventDefault();
+      score++;
+      scoreBoard.textContent = "Score: " + score;
+      moveTarget(target);
+      return false;
+    });
+    gameArea.appendChild(target);
+    moveTarget(target);
+    array.push(i[0])
+  }
+}
+
+function moveTarget(target) {
   const gameAreaRect = gameArea.getBoundingClientRect();
   const maxX = gameAreaRect.width - target.offsetWidth;
   const maxY = gameAreaRect.height - target.offsetHeight;
@@ -16,13 +39,6 @@ function moveTarget() {
   target.style.top = `${randomY}px`;
 }
 
-target.addEventListener('contextmenu', function(event) {
-  event.preventDefault();
-  score++;
-  scoreBoard.textContent = "Score: " + score;
-  moveTarget();
-  return false;
-});
 
 document.addEventListener("keydown", function(event) {
   if (event.ctrlKey && event.key.toLowerCase() === "o") {
@@ -31,6 +47,3 @@ document.addEventListener("keydown", function(event) {
     event.preventDefault();
   }
 });
-
-// Initial target position
-moveTarget();
